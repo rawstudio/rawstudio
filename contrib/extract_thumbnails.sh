0a73169a77bf1d346d0101001f8de4db5da6084c
akv <anders@kvistmail.dk>
2006-03-06 19:42:47 +0000

    changed a static path to /home/akv/Projects/rawstudio/contrib :)

--- contrib/extract_thumbnails.sh
+++ contrib/extract_thumbnails.sh
@@ -4,7 +4,7 @@ if test -x /usr/bin/gnome-raw-thumbnailer
 then
     for i in *.cr2 *.CR2 *.tif *.TIF *.orf *.ORF *.nef *.NEF
       do 
-      gnome-raw-thumbnailer -s 128 file:///home/akv/Projects/rawstudio/src/raws/$i $i.png
+      gnome-raw-thumbnailer -s 128 file://`pwd`/$i $i.png
     done 
 else
     echo "Install gnome-raw-thumbnailer!"
