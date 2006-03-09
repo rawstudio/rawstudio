3a619b0a60ada156a1763701f4b7e63df9aa0859
akv <anders@kvistmail.dk>
2006-03-09 12:41:40 +0000

    changed extract thumbnail to show some info when working and go drop the thumbnails into .rawstudio/file.raw_thumb.png

--- contrib/extract_thumbnails.sh
+++ contrib/extract_thumbnails.sh
@@ -2,10 +2,16 @@
 
 if test -x /usr/bin/gnome-raw-thumbnailer
 then
+    if ! test -d .rawstudio
+	then mkdir .rawstudio
+    fi
+    echo -n "Extracting thumbnails"
     for i in *.cr2 *.CR2 *.tif *.TIF *.orf *.ORF *.nef *.NEF
       do 
-      gnome-raw-thumbnailer -s 128 file://`pwd`/$i $i.png
+      gnome-raw-thumbnailer -s 128 file://`pwd`/$i .rawstudio/$i_thumb.png 2>&1 > /dev/null
+      echo -n "."
     done 
+    echo "done."
 else
     echo "Install gnome-raw-thumbnailer!"
 fi
