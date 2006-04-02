c8b41c6e2e26ea1831c5d9bc599f16d168797444
Anders Brander <anders@brander.dk>
2006-04-02 12:03:54 +0000

    .orf file extension added for Olympus.

--- src/rawstudio.c
+++ src/rawstudio.c
@@ -21,6 +21,7 @@ static RS_FILETYPE filetypes[] = {
 	{"crw", rs_load_raw_from_file, rs_thumb_grt},
 	{"nef", rs_load_raw_from_file, rs_thumb_grt},
 	{"tif", rs_load_raw_from_file, rs_thumb_grt},
+	{"orf", rs_load_raw_from_file, rs_thumb_grt},
 	{"jpg", rs_load_gdk, rs_thumb_gdk},
 	{NULL, NULL}
 };
