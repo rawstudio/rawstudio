83ac89acf97f6246558a9013e9e01aea6472f45a
Anders Brander <anders@brander.dk>
2006-05-21 00:42:10 +0000

    Added native loader for cr2-thumbnails.

--- src/tiff-meta.h
+++ src/tiff-meta.h
@@ -2,3 +2,4 @@
 #define ENDIANSWAP2(a) (((a) & 0x00FF) << 8) | (((a) & 0xFF00) >> 8)
 
 void rs_tiff_load_meta(const gchar *filename, RS_METADATA *meta);
+GdkPixbuf *rs_tiff_load_thumb(const gchar *src);
