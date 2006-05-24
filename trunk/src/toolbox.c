a1d8b9c3b6100a75db52b5908cd30e8022fc976f
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-05-24 03:01:44 +0000

    Declaration cleanup.

--- trunk/src/toolbox.c
+++ trunk/src/toolbox.c
@@ -15,7 +15,26 @@ struct reset_carrier {
 
 GtkLabel *infolabel;
 
+GtkWidget *gui_hist(RS_BLOB *rs, const gchar *label);
+GtkWidget *gui_box(const gchar *title, GtkWidget *in);
+void gui_transform_rot90_clicked(GtkWidget *w, RS_BLOB *rs);
+void gui_transform_rot180_clicked(GtkWidget *w, RS_BLOB *rs);
+void gui_transform_rot270_clicked(GtkWidget *w, RS_BLOB *rs);
+void gui_transform_mirror_clicked(GtkWidget *w, RS_BLOB *rs);
+void gui_transform_flip_clicked(GtkWidget *w, RS_BLOB *rs);
+GtkWidget *gui_transform(RS_BLOB *rs);
+GtkWidget *gui_tool_rgb_mixer(RS_BLOB *rs, gint n);
+GtkWidget *gui_tool_warmth(RS_BLOB *rs, gint n);
+GtkWidget *gui_slider(GtkObject *adj, const gchar *label);
 gboolean gui_adj_reset_callback(GtkWidget *widget, GdkEventButton *event, struct reset_carrier *rc);
+GtkWidget *gui_make_scale_from_adj(RS_BLOB *rs, GCallback cb, GtkObject *adj, guint mask);
+GtkWidget *gui_tool_exposure(RS_BLOB *rs, gint n);
+GtkWidget *gui_tool_saturation(RS_BLOB *rs, gint n);
+GtkWidget *gui_tool_hue(RS_BLOB *rs, gint n);
+GtkWidget *gui_tool_contrast(RS_BLOB *rs, gint n);
+GtkWidget *gui_tool_gamma(RS_BLOB *rs, gint n);
+GtkWidget *gui_make_tools(RS_BLOB *rs, gint n);
+void gui_notebook_callback(GtkNotebook *notebook, GtkNotebookPage *page, guint page_num, RS_BLOB *rs);
 
 GtkWidget *
 gui_hist(RS_BLOB *rs, const gchar *label)
