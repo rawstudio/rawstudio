77249892757dddfbc3d03055809b46da5151da5c
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-05-24 03:04:09 +0000

    Declaration cleanup.

--- trunk/src/drawingarea.c
+++ trunk/src/drawingarea.c
@@ -7,6 +7,11 @@
 
 gint start_x, start_y;
 
+gboolean drawingarea_expose (GtkWidget *widget, GdkEventExpose *event, RS_BLOB *rs);
+gboolean drawingarea_configure (GtkWidget *widget, GdkEventExpose *event, RS_BLOB *rs);
+gboolean gui_drawingarea_move_callback(GtkWidget *widget, GdkEventMotion *event, RS_BLOB *rs);
+gboolean gui_drawingarea_button(GtkWidget *widget, GdkEventButton *event, RS_BLOB *rs);
+
 gboolean
 drawingarea_expose (GtkWidget *widget, GdkEventExpose *event, RS_BLOB *rs)
 {
