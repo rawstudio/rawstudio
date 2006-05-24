8e2e061709b6a6d2717322ea221ab8f22e70db01
Anders Brander <anders@brander.dk>
2006-05-24 03:04:09 +0000

    Declaration cleanup.

--- src/drawingarea.h
+++ src/drawingarea.h
@@ -1,5 +1,1 @@
-gboolean drawingarea_expose (GtkWidget *widget, GdkEventExpose *event, RS_BLOB *rs);
-gboolean drawingarea_configure (GtkWidget *widget, GdkEventExpose *event, RS_BLOB *rs);
-gboolean gui_drawingarea_move_callback(GtkWidget *widget, GdkEventMotion *event, RS_BLOB *rs);
-gboolean gui_drawingarea_button(GtkWidget *widget, GdkEventButton *event, RS_BLOB *rs);
 GtkWidget *gui_drawingarea_make(RS_BLOB *rs);
