c3fa49dc2370e57ec989a5310a3b56a427dc4630
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-06-22 14:11:20 +0000

    Added gui_dialog_make_from_text() and gui_dialog_make_from_widget().

--- trunk/src/gtk-interface.c
+++ trunk/src/gtk-interface.c
@@ -63,6 +63,8 @@ void gui_menu_paste_callback(gpointer callback_data, guint callback_action, GtkW
 void gui_menu_copy_callback(gpointer callback_data, guint callback_action, GtkWidget *widget);
 GtkWidget *gui_make_menubar(RS_BLOB *rs, GtkWidget *window, GtkListStore *store, GtkWidget *iconbox, GtkWidget *toolbox);
 GtkWidget *gui_window_make(RS_BLOB *rs);
+GtkWidget *gui_dialog_make_from_text(const gchar *stock_id, gchar *primary_text, gchar *secondary_text);
+GtkWidget *gui_dialog_make_from_widget(const gchar *stock_id, gchar *primary_text, GtkWidget *widget);
 
 
 void
@@ -1043,6 +1045,57 @@ gui_window_make(RS_BLOB *rs)
 	gtk_window_set_title (GTK_WINDOW (window), _("Rawstudio"));
 	g_signal_connect((gpointer) window, "delete_event", G_CALLBACK(rs_shutdown), rs);
 	return(window);
+}
+
+GtkWidget *
+gui_dialog_make_from_text(const gchar *stock_id, gchar *primary_text, gchar *secondary_text)
+{
+	GtkWidget *secondary_label;
+
+	secondary_label = gtk_label_new (NULL);
+	gtk_label_set_line_wrap (GTK_LABEL (secondary_label), TRUE);
+	gtk_label_set_use_markup (GTK_LABEL (secondary_label), TRUE);
+	gtk_misc_set_alignment (GTK_MISC (secondary_label), 0.0, 0.5);
+	gtk_label_set_selectable (GTK_LABEL (secondary_label), TRUE);
+	gtk_label_set_markup (GTK_LABEL (secondary_label), secondary_text);
+	
+	return(gui_dialog_make_from_widget(stock_id, primary_text, secondary_label));
+}
+
+GtkWidget *
+gui_dialog_make_from_widget(const gchar *stock_id, gchar *primary_text, GtkWidget *widget)
+{
+	GtkWidget *dialog, *image, *hbox, *vbox;
+	GtkWidget *primary_label;
+
+	image = gtk_image_new_from_stock(stock_id, GTK_ICON_SIZE_DIALOG);
+	gtk_misc_set_alignment (GTK_MISC (image), 0.5, 0.0);
+	dialog = gtk_dialog_new();
+	gtk_container_set_border_width (GTK_CONTAINER (dialog), 5);
+	gtk_box_set_spacing (GTK_BOX (GTK_DIALOG (dialog)->vbox), 14);
+	gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
+	gtk_dialog_set_has_separator (GTK_DIALOG (dialog), FALSE);
+	gtk_window_set_title (GTK_WINDOW (dialog), "");
+	gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
+	gtk_window_set_destroy_with_parent (GTK_WINDOW (dialog), TRUE);
+
+	primary_label = gtk_label_new (NULL);
+	gtk_label_set_line_wrap (GTK_LABEL (primary_label), TRUE);
+	gtk_label_set_use_markup (GTK_LABEL (primary_label), TRUE);
+	gtk_misc_set_alignment (GTK_MISC (primary_label), 0.0, 0.5);
+	gtk_label_set_selectable (GTK_LABEL (primary_label), TRUE);
+	gtk_label_set_markup (GTK_LABEL (primary_label), primary_text);
+
+	hbox = gtk_hbox_new (FALSE, 12);
+	gtk_container_set_border_width (GTK_CONTAINER (hbox), 5);
+	gtk_box_pack_start (GTK_BOX (hbox), image, FALSE, FALSE, 0);
+	vbox = gtk_vbox_new (FALSE, 12);
+	gtk_box_pack_start (GTK_BOX (hbox), vbox, FALSE, FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (vbox), primary_label, FALSE, FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (vbox), widget, FALSE, FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), hbox, FALSE, FALSE, 0);
+
+	return(dialog);
 }
 
 int
