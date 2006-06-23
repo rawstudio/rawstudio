5c43f723a9c5b071cf2477c5736688695b73fcfb
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-06-23 14:15:20 +0000

    Changed rs_conf_*_boolean() to behave well and added declarations.

--- trunk/src/conf_interface.h
+++ trunk/src/conf_interface.h
@@ -4,6 +4,7 @@
 #define CONF_GAMMAVALUE "gamma"
 #define CONF_PASTE_MASK "paste_mask"
 #define CONF_DEFAULT_EXPORT_TEMPLATE "default_export_template"
+#define CONF_CACHEDIR_IS_LOCAL "cache_in_home"
 
 // get the last working directory from gconf
 void rs_set_last_working_directory(const char *lwd);
@@ -11,6 +12,8 @@ void rs_set_last_working_directory(const char *lwd);
 // save the current working directory to gconf
 gchar *rs_get_last_working_directory(void);
 
+gboolean rs_conf_get_boolean(const gchar *name, gboolean *boolean_value);
+gboolean rs_conf_set_boolean(const gchar *name, gboolean bool_value);
 gchar *rs_conf_get_string(const gchar *path);
 gboolean rs_conf_set_string(const gchar *path, const gchar *string);
 gboolean rs_conf_get_integer(const gchar *name, gint *integer_value);
