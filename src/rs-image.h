9a8361179b7c63bbc666ee3b5ce4f5ce6d657b37
akv <anders@kvistmail.dk>
2006-07-02 17:33:45 +0000

    Reverted to 455 and changed header in all source files to a real GPL header

--- src/rs-image.h
+++ src/rs-image.h
@@ -1,11 +1,20 @@
 /*
- * RAWstudio - Rawstudio is an open source raw-image converter written in GTK+.
- * by Anders BRander <anders@brander.dk> and Anders Kvist <akv@lnxbx.dk>
+ * Copyright (C) 2006 Anders Brander <anders@brander.dk> and 
+ * Anders Kvist <akv@lnxbx.dk>
  *
- * rs-image.h - image functions
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * Rawstudio is licensed under the GNU General Public License.
- * It uses DCRaw and UFraw code to do the actual raw decoding.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 RS_IMAGE16 *rs_image16_new(const guint width, const guint height, const guint channels, const guint pixelsize);
