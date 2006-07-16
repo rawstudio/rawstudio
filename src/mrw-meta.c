73b096ac363f468c30496bfdbfb1558c47e16507
Anders Brander <anders@brander.dk>
2006-07-16 21:21:04 +0000

    Now using rs_tiff_load_thumb() for .orf-files.

--- src/mrw-meta.c
+++ src/mrw-meta.c
@@ -112,6 +112,7 @@ rs_mrw_load_thumb(const gchar *src)
 		}
 	}
 
+	meta.orientation = 0;
 	meta.thumbnail_start = 0;
 	meta.thumbnail_length = 0;
 	meta.preview_start = 0;
@@ -152,13 +153,13 @@ rs_mrw_load_thumb(const gchar *src)
 		g_free(thumbbuffer);
 		
 		if (pixbuf==NULL) return(NULL);
-		if ((gdk_pixbuf_get_width(pixbuf) == 160) && (gdk_pixbuf_get_height(pixbuf)==120))
+/*		if ((gdk_pixbuf_get_width(pixbuf) == 160) && (gdk_pixbuf_get_height(pixbuf)==120))
 		{
 			pixbuf2 = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, 160, 106);
 			gdk_pixbuf_copy_area(pixbuf, 0, 7, 160, 106, pixbuf2, 0, 0);
 			g_object_unref(pixbuf);
 			pixbuf = pixbuf2;
-		}
+		}*/
 		ratio = ((gdouble) gdk_pixbuf_get_width(pixbuf))/((gdouble) gdk_pixbuf_get_height(pixbuf));
 		if (ratio>1.0)
 			pixbuf2 = gdk_pixbuf_scale_simple(pixbuf, 128, (gint) (128.0/ratio), GDK_INTERP_BILINEAR);
