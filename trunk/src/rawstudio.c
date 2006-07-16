d58de206670fc2239b47d21ce1834e2ebbed3347
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-07-16 10:42:01 +0000

    Added rs_tiff_load_meta to nef-filetype.

--- trunk/src/rawstudio.c
+++ trunk/src/rawstudio.c
@@ -87,7 +87,7 @@ rs_load_gdk(gboolean new_value)
 static RS_FILETYPE filetypes[] = {
 	{"cr2", FILETYPE_RAW, rs_photo_open_dcraw, rs_tiff_load_thumb, rs_tiff_load_meta},
 	{"crw", FILETYPE_RAW, rs_photo_open_dcraw, rs_ciff_load_thumb, rs_ciff_load_meta},
-	{"nef", FILETYPE_RAW, rs_photo_open_dcraw, rs_tiff_load_thumb, NULL},
+	{"nef", FILETYPE_RAW, rs_photo_open_dcraw, rs_tiff_load_thumb, rs_tiff_load_meta},
 	{"mrw", FILETYPE_RAW, rs_photo_open_dcraw, rs_thumb_grt, NULL},
 	{"tif", FILETYPE_RAW, rs_photo_open_dcraw, rs_tiff_load_thumb, rs_tiff_load_meta},
 	{"orf", FILETYPE_RAW, rs_photo_open_dcraw, rs_thumb_grt, NULL},
