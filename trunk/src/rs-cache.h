f8b1bbc637b23d47000cd0cd1b22b02f93ba2838
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-07-31 21:49:34 +0000

    Changed rs_cache_load() to return gboolean.

--- trunk/src/rs-cache.h
+++ trunk/src/rs-cache.h
@@ -19,5 +19,5 @@
 
 gchar *rs_cache_get_name(const gchar *src);
 void rs_cache_save(RS_PHOTO *photo);
-void rs_cache_load(RS_PHOTO *photo);
+gboolean rs_cache_load(RS_PHOTO *photo);
 void rs_cache_load_quick(const gchar *filename, gint *priority);
