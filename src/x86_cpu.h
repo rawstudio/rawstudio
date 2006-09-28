eefda413a7bb91686a07392e6a25effcaee591a3
Anders Brander <anders@brander.dk>
2006-09-28 18:38:26 +0000

    Added cpu-specific-header from Edouard Gomez.

---
+++ src/x86_cpu.h
@@ -0,0 +1,40 @@
+#ifndef __X86__CPU_H__
+#define __X86__CPU_H__
+
+#if defined(__x86_64__)
+#  define REG_a "rax"
+#  define REG_b "rbx"
+#  define REG_c "rcx"
+#  define REG_d "rdx"
+#  define REG_D "rdi"
+#  define REG_S "rsi"
+#  define PTR_SIZE "8"
+
+#  define REG_SP "rsp"
+#  define REG_BP "rbp"
+#  define REGBP   rbp
+#  define REGa    rax
+#  define REGb    rbx
+#  define REGc    rcx
+#  define REGSP   rsp
+
+#else
+
+#  define REG_a "eax"
+#  define REG_b "ebx"
+#  define REG_c "ecx"
+#  define REG_d "edx"
+#  define REG_D "edi"
+#  define REG_S "esi"
+#  define PTR_SIZE "4"
+
+#  define REG_SP "esp"
+#  define REG_BP "ebp"
+#  define REGBP   ebp
+#  define REGa    eax
+#  define REGb    ebx
+#  define REGc    ecx
+#  define REGSP   esp
+#endif
+
+#endif
