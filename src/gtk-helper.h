8825b375050c77658b528e8a202811bc5ffe11f2
Anders Brander <anders@brander.dk>
2006-10-12 17:22:28 +0000

    Now remembers last saved file format.

--- src/gtk-helper.h
+++ src/gtk-helper.h
@@ -20,6 +20,7 @@
 const RS_FILETYPE *gui_filetype_combobox_get_filetype(GtkComboBox *widget);
 const gchar *gui_filetype_combobox_get_ext(GtkComboBox *widget);
 GtkWidget *gui_filetype_combobox();
+void gui_filetype_combobox_set_active(GtkWidget *combo, RS_FILETYPE *set);
 void checkbox_set_conf(GtkToggleButton *togglebutton, gpointer user_data);
 GtkWidget *checkbox_from_conf(const gchar *conf, gchar *label, gboolean default_value);
 GtkWidget *gui_tooltip_no_window(GtkWidget *widget, gchar *tip_tip, gchar *tip_private);
