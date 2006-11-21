6044e78db8dec8ac9189db6dd11c80ffde19146e
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-11-21 18:01:05 +0000

    Removed rs_image16_affine().

--- trunk/src/rs-image.h
+++ trunk/src/rs-image.h
@@ -31,7 +31,6 @@ void rs_image8_free(RS_IMAGE8 *rsi);
 void rs_image16_orientation(RS_IMAGE16 *rsi, gint orientation);
 RS_IMAGE16 *rs_image16_transform(RS_IMAGE16 *in, RS_IMAGE16 *out, RS_MATRIX3 *inverse_affine,
 	RS_RECT *crop, gint width, gint height, gboolean keep_aspect, gdouble scale, gdouble angle, gint orientation);
-RS_IMAGE16 *rs_image16_affine(RS_IMAGE16 *in, RS_IMAGE16 *out, RS_MATRIX3 *affine, RS_MATRIX3 *inverse_affine, RS_RECT *crop);
 RS_IMAGE16 *rs_image16_scale_double(RS_IMAGE16 *in, RS_IMAGE16 *out, gdouble scale);
 RS_IMAGE16 *rs_image16_copy(RS_IMAGE16 *rsi);
 void rs_image16_crop(RS_IMAGE16 **rsi, RS_RECT *rect);
