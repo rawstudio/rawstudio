1c61fbd7896e64aa82898b821a4d74395b73c61b
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2006-11-21 18:35:39 +0000

    Added rs_image16_transform_getwh().

--- trunk/src/rs-image.h
+++ trunk/src/rs-image.h
@@ -29,6 +29,7 @@ void rs_image16_free(RS_IMAGE16 *rsi);
 RS_IMAGE8 *rs_image8_new(const guint width, const guint height, const guint channels, const guint pixelsize);
 void rs_image8_free(RS_IMAGE8 *rsi);
 void rs_image16_orientation(RS_IMAGE16 *rsi, gint orientation);
+void rs_image16_transform_getwh(RS_IMAGE16 *in, RS_RECT *crop, gdouble angle, gint orientation, gint *w, gint *h);
 RS_IMAGE16 *rs_image16_transform(RS_IMAGE16 *in, RS_IMAGE16 *out, RS_MATRIX3 *inverse_affine,
 	RS_RECT *crop, gint width, gint height, gboolean keep_aspect, gdouble scale, gdouble angle, gint orientation);
 RS_IMAGE16 *rs_image16_scale_double(RS_IMAGE16 *in, RS_IMAGE16 *out, gdouble scale);
