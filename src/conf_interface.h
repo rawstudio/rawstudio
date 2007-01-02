a75bc290d2bb6845aa65079c89d1360183c526af
akv <anders@kvistmail.dk>
2007-01-02 23:48:31 +0000

    Added combobox for selecting grid in crop tool frame.

--- src/conf_interface.h
+++ src/conf_interface.h
@@ -41,6 +41,7 @@
 #define CONF_CMS_DI_PROFILE_SELECTED "cms_di_profile_selected"
 #define CONF_CMS_EX_PROFILE_LIST "cms_ex_profile_list"
 #define CONF_CMS_EX_PROFILE_SELECTED "cms_ex_profile_selected"
+#define CONF_ROI_GRID "roi_grid"
 
 
 #define DEFAULT_CONF_EXPORT_DIRECTORY "exports/"
