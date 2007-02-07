a32d1062e845bb65def9c74db35f547b10087076
Anders Brander <anders@brander.dk>
2007-02-07 16:52:35 +0000

    Made rs_filetype_get() use rs_conf_get_boolean() instead of rs_conf_get_boolean_with_default().

--- src/rawstudio.c
+++ src/rawstudio.c
@@ -1115,7 +1115,8 @@ rs_filetype_get(const gchar *filename, gboolean load)
 	RS_FILETYPE *filetype = filetypes;
 	gchar *iname;
 	gint n;
-	rs_conf_get_boolean_with_default(CONF_LOAD_GDK, &load_gdk, FALSE);
+	gboolean load_gdk = FALSE;
+	rs_conf_get_boolean(CONF_LOAD_GDK, &load_gdk);
 	iname = g_ascii_strdown(filename,-1);
 	n = 0;
 	while(filetype)
