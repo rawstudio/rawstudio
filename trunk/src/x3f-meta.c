d61e6ef077bb8dd80a94089b569340cc15b560d2
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2007-07-19 13:20:03 +0000

    Now including glib.h instead of gtk.h.

--- trunk/src/x3f-meta.c
+++ trunk/src/x3f-meta.c
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#include <gtk/gtk.h>
+#include <glib.h>
 #include "rawstudio.h"
 #include "rawfile.h"
 #include "x3f-meta.h"
