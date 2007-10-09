b1a6cdf3e346f6b0324d9cc2b026a3c67eabb76a
Anders Brander <anders@brander.dk>
2007-10-09 17:01:07 +0000

    Added EogPixbufCellRenderer from Eye of Gnome 2.20.0.

---
+++ src/eog-pixbuf-cell-renderer.h
@@ -0,0 +1,51 @@
+/* Eye Of Gnome - Pixbuf Cellrenderer 
+ *
+ * Lifted from eog-2.20.0 with minor changes, Thanks! - Anders Brander
+ *
+ * Copyright (C) 2007 The GNOME Foundation
+ *
+ * Author: Lucas Rocha <lucasr@gnome.org>
+ *
+ * Based on gnome-control-center code (capplets/appearance/wp-cellrenderer.c) by: 
+ *      - Denis Washington <denisw@svn.gnome.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __EOG_PIXBUF_CELL_RENDERER_H__
+#define __EOG_PIXBUF_CELL_RENDERER_H__
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+typedef struct _EogPixbufCellRenderer EogPixbufCellRenderer;
+typedef struct _EogPixbufCellRendererClass EogPixbufCellRendererClass;
+
+struct _EogPixbufCellRenderer
+{
+	GtkCellRendererPixbuf parent;
+};
+
+struct _EogPixbufCellRendererClass
+{
+	GtkCellRendererPixbufClass parent;
+};
+
+GtkCellRenderer *eog_pixbuf_cell_renderer_new (void);
+
+G_END_DECLS
+
+#endif /* __EOG_PIXBUF_CELL_RENDERER_H__ */
