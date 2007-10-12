803f6334bb0afcfeb2ee4b467cf00328259c563a
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2007-10-12 17:23:05 +0000

    Fixed rounding error in rs_curve_draw_spline().

--- trunk/src/rs-curve.c
+++ trunk/src/rs-curve.c
@@ -565,7 +565,7 @@ rs_curve_draw_spline(GtkWidget *widget)
 
 	for (i=0; i<width; i++)
 	{
-		gint y = (gint)(height*(1-samples[i]));
+		gint y = (gint)(height*(1-samples[i])+0.5);
 		gdk_draw_point(window, gc, i, y);
 	}
 
