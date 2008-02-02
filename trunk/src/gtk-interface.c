9bb85b75aed76998db550aced74b989fb78f4771
akv <akv@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2008-02-02 15:38:58 +0000

    Using \n instead of , to seperate translators in translator credits.

--- trunk/src/gtk-interface.c
+++ trunk/src/gtk-interface.c
@@ -1115,7 +1115,7 @@ gui_about(void)
 	gtk_show_about_dialog(GTK_WINDOW(rawstudio_window),
 		"authors", authors,
 		"artists", artists,
-		"translator-credits", "Simone Contini, Paweł Gołaszewski, Alexandre Prokoudine, Jakub Friedl, Carsten Mathaes",
+		"translator-credits", "Simone Contini\nPaweł Gołaszewski\nAlexandre Prokoudine\nJakub Friedl\nCarsten Mathaes",
 		"comments", _("A raw image converter for GTK+/GNOME"),
 		"version", VERSION,
 		"website", "http://rawstudio.org/",
