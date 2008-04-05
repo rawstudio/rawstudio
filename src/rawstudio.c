79e33354934dd6ff36b6cf4eb473bad3989fcfd5
akv <anders@kvistmail.dk>
2008-04-05 19:08:38 +0000

    Added support for Kodak DCR files.

--- src/rawstudio.c
+++ src/rawstudio.c
@@ -103,6 +103,8 @@ rs_init_filetypes(void)
 		rs_photo_open_dcraw, rs_tiff_load_thumb, rs_tiff_load_meta, NULL);
 	rs_add_filetype("kdc", FILETYPE_RAW, ".kdc", _("Kodak"),
 		rs_photo_open_dcraw, rs_tiff_load_thumb, rs_tiff_load_meta, NULL);
+	rs_add_filetype("kdc", FILETYPE_RAW, ".dcr", _("Kodak"),
+		rs_photo_open_dcraw, rs_tiff_load_thumb, rs_tiff_load_meta, NULL);
 	rs_add_filetype("x3f", FILETYPE_RAW, ".x3f", _("Sigma"),
 		rs_photo_open_dcraw, rs_x3f_load_thumb, NULL, NULL);
 	rs_add_filetype("orf", FILETYPE_RAW, ".orf", "",
