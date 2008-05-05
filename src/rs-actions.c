299587745eaac40f569b487c320623b3e583446a
akv <anders@kvistmail.dk>
2008-05-05 19:01:05 +0000

    Added Dutch translation by Olav Lavell.

--- src/rs-actions.c
+++ src/rs-actions.c
@@ -760,7 +760,7 @@ ACTION(about)
 	gtk_show_about_dialog(GTK_WINDOW(rawstudio_window),
 		"authors", authors,
 		"artists", artists,
-		"translator-credits", "Simone Contini\nPaweł Gołaszewski\nAlexandre Prokoudine\nJakub Friedl\nCarsten Mathaes\nEdouard Gomez\nMartin Egger\nKrzysztof Kościuszkiewicz\nEinar Ryeng\nOlli Hänninen\nCarlos Dávila\nPatrik Jarl",
+		"translator-credits", "Simone Contini\nPaweł Gołaszewski\nAlexandre Prokoudine\nJakub Friedl\nCarsten Mathaes\nEdouard Gomez\nMartin Egger\nKrzysztof Kościuszkiewicz\nEinar Ryeng\nOlli Hänninen\nCarlos Dávila\nPatrik Jarl\nOlav Lavell",
 		"comments", _("A raw image converter for GTK+/GNOME"),
 		"version", VERSION,
 		"website", "http://rawstudio.org/",
