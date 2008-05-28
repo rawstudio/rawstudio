9d51ffef65b6551d3bbb590023bbff7bb0a6f6b5
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2008-05-28 20:59:50 +0000

    Fixed colors when exporting with CMS enabled.

--- trunk/src/rawstudio.c
+++ trunk/src/rawstudio.c
@@ -511,10 +511,10 @@ rs_photo_save(RS_PHOTO *photo, const gchar *filename, gint filetype, gint width,
 
 	/* Initialize color transform */
 	rct = rs_color_transform_new();
+	rs_color_transform_set_cms_transform(rct, transform);
 	rs_color_transform_set_adobe_matrix(rct, &photo->metadata->adobe_coeff);
 	rs_color_transform_set_from_settings(rct, photo->settings[snapshot], MASK_ALL);
 	rs_color_transform_set_output_format(rct, 8);
-	rs_color_transform_set_cms_transform(rct, transform);
 
 	/* actually save */
 	switch (filetype)
