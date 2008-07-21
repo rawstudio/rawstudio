a5b23ab78bb28d049060a4013e44abfa4f18701d
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2008-07-21 21:37:28 +0000

    Added rs_atof().

---
+++ trunk/src/rs-utils.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2006-2008 Anders Brander <anders@brander.dk> and 
+ * Anders Kvist <akv@lnxbx.dk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef RS_UTILS_H
+#define RS_UTILS_H
+
+#include <glib.h>
+
+/**
+ * A version of atof() that isn't locale specific
+ * @note This doesn't do any error checking!
+ * @param str A NULL terminated string representing a number
+ * @return The number represented by str or 0.0 if str is NULL
+ */
+extern gdouble rs_atof(const gchar *str);
+
+#endif /* RS_UTILS_H */
