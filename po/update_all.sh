6a51ac6025417ac091ff340f8166b6b440abb608
akv <anders@kvistmail.dk>
2009-01-11 03:30:45 +0000

    Added script to update all lang files.

---
+++ po/update_all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm rawstudio.pot
+make rawstudio.pot
+
+for FILE in *.po; do
+    intltool-update -d $(echo ${FILE}|cut -d"." -f1)
+done
+
+# Set a few headerlines in all .po files
+sed -i 's/\"Project-Id-Version:.*\"/\"Project-Id-Version: Rawstudio 1.1.1\\n\"/g' *.po
+sed -i 's/\"Report-Msgid-Bugs-To:.*\"/\"Report-Msgid-Bugs-To: rawstudio-dev@rawstudio.org\\n\"/g' *.po
+sed -i 's/\"Language-Team:.*\"/\"Language-Team: Rawstudio development <rawstudio-dev@rawstudio.org>\\n\"/g' *.po
