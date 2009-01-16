cc447f12698745146880e111d55f29657ddc8200
Anders Brander <anders@brander.dk>
2009-01-16 01:03:15 +0000

    Added signal throttling to RSCache.

--- plugins/cache/cache.c
+++ plugins/cache/cache.c
@@ -33,6 +33,8 @@ struct _RSCache {
 	RSFilter parent;
 
 	RS_IMAGE16 *image;
+	gboolean ignore_changed;
+	gint latency;
 };
 
 struct _RSCacheClass {
@@ -41,6 +43,13 @@ struct _RSCacheClass {
 
 RS_DEFINE_FILTER(rs_cache, RSCache)
 
+enum {
+	PROP_0,
+	PROP_LATENCY
+};
+
+static void get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec);
+static void set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec);
 static RS_IMAGE16 *get_image(RSFilter *filter);
 static void previous_changed(RSFilter *filter, RSFilter *parent);
 
@@ -54,6 +63,17 @@ static void
 rs_cache_class_init(RSCacheClass *klass)
 {
 	RSFilterClass *filter_class = RS_FILTER_CLASS (klass);
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
+	object_class->get_property = get_property;
+	object_class->set_property = set_property;
+
+	g_object_class_install_property(object_class,
+		PROP_LATENCY, g_param_spec_int(
+			"latency", "latency", "Signal propagation latency in milliseconds, this can be used to reduce signals from \"noisy\" filters.",
+			0, 10000, 1,
+			G_PARAM_READWRITE)
+	);
 
 	filter_class->name = "Listen for changes and caches image data";
 	filter_class->get_image = get_image;
@@ -64,6 +84,38 @@ static void
 rs_cache_init(RSCache *cache)
 {
 	cache->image = NULL;
+	cache->ignore_changed = FALSE;
+	cache->latency = 1;
+}
+
+static void
+get_property(GObject *object, guint property_id, GValue *value, GParamSpec *pspec)
+{
+	RSCache *cache = RS_CACHE(object);
+
+	switch (property_id)
+	{
+		case PROP_LATENCY:
+			g_value_set_int(value, cache->latency);
+			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+	}
+}
+
+static void
+set_property(GObject *object, guint property_id, const GValue *value, GParamSpec *pspec)
+{
+	RSCache *cache = RS_CACHE(object);
+
+	switch (property_id)
+	{
+		case PROP_LATENCY:
+			cache->latency = g_value_get_int(value);
+			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+	}
 }
 
 static RS_IMAGE16 *
@@ -77,6 +129,16 @@ get_image(RSFilter *filter)
 	return (cache->image) ? g_object_ref(cache->image) : NULL;
 }
 
+static gboolean
+previous_changed_timeout_func(gpointer data)
+{
+	RS_CACHE(data)->ignore_changed = FALSE;
+
+	rs_filter_changed(RS_FILTER(data));
+
+	return FALSE;
+}
+
 static void
 previous_changed(RSFilter *filter, RSFilter *parent)
 {
@@ -87,6 +149,14 @@ previous_changed(RSFilter *filter, RSFilter *parent)
 
 	cache->image = NULL;
 
-	/* Propagate */
-	rs_filter_changed(filter);
+	if (cache->latency > 0)
+	{
+		if (!cache->ignore_changed)
+		{
+			cache->ignore_changed = TRUE;
+			g_timeout_add(cache->latency, previous_changed_timeout_func, cache);
+		}
+	}
+	else
+		rs_filter_changed(filter);
 }
