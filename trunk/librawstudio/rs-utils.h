59867c515ba950de2ddc1c12f1a5a07c3fb9ca2f
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2009-04-02 08:01:31 +0000

    Changed CanonEv() to use floats.

--- trunk/librawstudio/rs-utils.h
+++ trunk/librawstudio/rs-utils.h
@@ -148,7 +148,7 @@ rs_object_class_property_reset(GObject *object, const gchar *property_name);
 extern void
 check_install();
 
-extern gint 
+extern gfloat
 CanonEv(gint val);
 
 #endif /* RS_UTILS_H */
