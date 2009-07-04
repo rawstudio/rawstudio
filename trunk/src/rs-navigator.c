e69bc868d1d7aeacd50d7f9a32055f035f80033d
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2009-07-04 21:15:27 +0000

    Fixed rs_navigator_set_adjustments() to read initial values from adjusters.

--- trunk/src/rs-navigator.c
+++ trunk/src/rs-navigator.c
@@ -92,6 +92,11 @@ rs_navigator_set_adjustments(RSNavigator *navigator, GtkAdjustment *vadjustment,
 	navigator->vadjustment = g_object_ref(vadjustment);
 	navigator->hadjustment = g_object_ref(hadjustment);
 
+	navigator->width = (gint) (gtk_adjustment_get_upper(hadjustment)+0.5);
+	navigator->height = (gint) (gtk_adjustment_get_upper(vadjustment)+0.5);
+	navigator->x = (gint) (gtk_adjustment_get_value(hadjustment)+0.5);
+	navigator->x = (gint) (gtk_adjustment_get_value(vadjustment)+0.5);
+
 	navigator->vadjustment_signal1 = g_signal_connect(vadjustment, "changed", G_CALLBACK(v_changed), navigator);
 	navigator->vadjustment_signal2 = g_signal_connect(vadjustment, "value-changed", G_CALLBACK(v_value_changed), navigator);
 	navigator->hadjustment_signal1 = g_signal_connect(hadjustment, "changed", G_CALLBACK(h_changed), navigator);
