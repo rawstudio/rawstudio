9e1582f220650d4b138f63d97609c5c572d95878
Anders Brander <anders@brander.dk>
2009-07-05 15:33:14 +0000

    Enabled GUI to select preloading.

--- src/gtk-interface.c
+++ src/gtk-interface.c
@@ -641,9 +641,7 @@ gui_make_preference_window(RS_BLOB *rs)
 	gtk_box_pack_start (GTK_BOX (preview_page), load_gdk_check, FALSE, TRUE, 0);
 
 	preload_check = checkbox_from_conf(CONF_PRELOAD, _("Preload photos"), FALSE);
-#ifdef EXPERIMENTAL
 	gtk_box_pack_start (GTK_BOX (preview_page), preload_check, FALSE, TRUE, 0);
-#endif
 	g_signal_connect ((gpointer) preload_check, "toggled",
 		G_CALLBACK (gui_preference_preload_changed), rs);
 
