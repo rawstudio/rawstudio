9035c2b28b5537869f6c5c104d1de559b868e51e
Anders Brander <anders@brander.dk>
2009-08-06 11:01:46 +0000

    Added safeguard against double linking in rs_filter_set_previous().

--- librawstudio/rs-filter.c
+++ librawstudio/rs-filter.c
@@ -124,15 +124,20 @@ rs_filter_set_previous(RSFilter *filter, RSFilter *previous)
 	g_assert(RS_IS_FILTER(filter));
 	g_assert(RS_IS_FILTER(previous));
 
-	if (filter->previous && (filter->previous != previous))
+	/* We will only set the previous filter if it differs from current previous filter */
+	if (filter->previous != previous)
 	{
-		filter->previous->next_filters = g_slist_remove(filter->previous->next_filters, filter);
-		g_object_unref(filter->previous);
+		if (filter->previous)
+		{
+			/* If we already got a previous filter, clean up */
+			filter->previous->next_filters = g_slist_remove(filter->previous->next_filters, filter);
+			g_object_unref(filter->previous);
+		}
+		else
+			filter->previous = g_object_ref(previous);
+
+		previous->next_filters = g_slist_append(previous->next_filters, filter);
 	}
-
-	filter->previous = g_object_ref(previous);
-
-	previous->next_filters = g_slist_append(previous->next_filters, filter);
 }
 
 /**
