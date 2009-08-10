3fab3f32fce76ba41e6d5ef67e9e95201cc96785
Anders Brander <anders@brander.dk>
2009-08-10 18:12:59 +0000

    Added Leica RWL file format.

--- plugins/meta-tiff/tiff-meta.c
+++ plugins/meta-tiff/tiff-meta.c
@@ -1458,6 +1458,7 @@ rs_plugin_load(RSPlugin *plugin)
 	rs_filetype_register_meta_loader(".nef", "Nikon NEF", tif_load_meta, 10);
 	rs_filetype_register_meta_loader(".nrw", "Nikon NEF 2", tif_load_meta, 10);
 	rs_filetype_register_meta_loader(".tif", "Canon TIFF", tif_load_meta, 10);
+	rs_filetype_register_meta_loader(".rwl", "Leica", tif_load_meta, 10);
 	rs_filetype_register_meta_loader(".arw", "Sony", sony_load_meta, 10);
 	rs_filetype_register_meta_loader(".sr2", "Sony", sony_load_meta, 10);
 	rs_filetype_register_meta_loader(".srf", "Sony", sony_load_meta, 10);
