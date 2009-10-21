d662258feb88659f379f79fd31d11ef801043a6a
abrander <abrander@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2009-10-21 01:52:00 +0000

    Added missing GPL header to rs-filter-param.*.

--- trunk/librawstudio/rs-filter-param.c
+++ trunk/librawstudio/rs-filter-param.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2006-2009 Anders Brander <anders@brander.dk> and 
+ * Anders Kvist <akv@lnxbx.dk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include "rs-filter-param.h"
 
 G_DEFINE_TYPE(RSFilterParam, rs_filter_param, G_TYPE_OBJECT)
