5446be6bf11d2204f0e7fa6265eff4ea0c2036aa
post <post@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2010-03-28 16:36:33 +0000

    Add header comment.

--- trunk/src/rs-store.h
+++ trunk/src/rs-store.h
@@ -220,6 +220,12 @@ rs_store_group_select_name(RSStore *store, const gchar *filename);
 extern void
 rs_store_group_ungroup_name(RSStore *store, const gchar *filename);
 
+/**
+ * Add a single image to the image store.
+ * Add an image with NULL as filename to update the iconview 
+ * @param store A RSStore
+ * @param fullname Full filename including path to the file.
+ */
 extern void
 rs_store_load_file(RSStore *store, gchar *fullname);
 
