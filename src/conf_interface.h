e5d8973941be83a0229cea72763fc6cd4922b26b
post <klauspost@gmail.com>
2010-04-05 10:37:27 +0000

    Remember Percent scale from one export to the next.

--- src/conf_interface.h
+++ src/conf_interface.h
@@ -65,6 +65,7 @@
 #define CONF_LIBRARY_AUTOTAG "library_autotag"
 #define CONF_LIBRARY_TAG_SEARCH "library_tag_search"
 #define CONF_EXPORT_AS_FOLDER "export_as_folder"
+#define CONF_EXPORT_AS_SIZE_PERCENT "export_as_size_percent"
 
 #define DEFAULT_CONF_EXPORT_FILENAME "%f_%2c"
 #define DEFAULT_CONF_BATCH_DIRECTORY "batch_exports/"
