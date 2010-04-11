801930ec4f90a3da6dd091cab49011502f9bd3c7
post <klauspost@gmail.com>
2010-04-11 11:59:49 +0000

    Since histogram and levels are usually the first updated, make them request "quick" images. This significantly reduces image load times.

--- src/rs-histogram.c
+++ src/rs-histogram.c
@@ -158,7 +158,7 @@ calculate_histogram(RSHistogramWidget *histogram)
 		return;
 
 	RSFilterRequest *request = rs_filter_request_new();
-	rs_filter_request_set_quick(RS_FILTER_REQUEST(request), FALSE);
+	rs_filter_request_set_quick(RS_FILTER_REQUEST(request), TRUE);
 	rs_filter_param_set_object(RS_FILTER_PARAM(request), "colorspace", histogram->display_color_space);
 		
 	RSFilterResponse *response = rs_filter_get_image8(histogram->input, request);
