4defcc355e1b720fa42f2dfe82970e4b9699bcbc
Anders Brander <anders@brander.dk>
2010-05-25 19:45:43 +0000

    [380] Now setting proper macro for realpath() (Patch from Andy Young).

--- librawstudio/rs-utils.c
+++ librawstudio/rs-utils.c
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#define _XOPEN_SOURCE /* strptime() */
+#define _XOPEN_SOURCE 500 /* strptime() and realpath() */
 #include <rawstudio.h>
 #include <config.h>
 #include <glib.h>
