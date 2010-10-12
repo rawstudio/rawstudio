2eacd9f0f344e1d4fb81dce812077ff4ad6bfa94
post <post@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2010-10-12 17:49:12 +0000

    Fix compilation issues.

--- trunk/librawstudio/rs-exif.h
+++ trunk/librawstudio/rs-exif.h
@@ -32,7 +32,6 @@ typedef void RS_IPTC_DATA;
 
 extern RS_EXIF_DATA *rs_exif_load_from_file(const gchar *);
 extern RS_EXIF_DATA *rs_exif_load_from_rawfile(RAWFILE *rawfile);
-extern void rs_exif_add_to_file(RS_EXIF_DATA *d, const gchar *filename);
 extern void rs_exif_free(RS_EXIF_DATA *d);
 extern gboolean rs_exif_copy(const gchar *input_filename, const gchar *output_filename, const gchar *color_space);
 
