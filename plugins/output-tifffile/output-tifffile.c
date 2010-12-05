7903343bc848a0a71ad608e1f999abde326b451a
post <klauspost@gmail.com>
2010-12-05 22:36:02 +0000

    Add EXIF to Tiff images.

--- plugins/output-tifffile/output-tifffile.c
+++ plugins/output-tifffile/output-tifffile.c
@@ -277,8 +277,7 @@ execute(RSOutput *output, RSFilter *filter)
 	gchar *input_filename = NULL;
 	rs_filter_get_recursive(filter, "filename", &input_filename, NULL);
 
-	/* FIXME: rs_exif_copy() is broken! */
-//	rs_exif_copy(input_filename, tifffile->filename);
+	rs_exif_copy(input_filename, tifffile->filename,  G_OBJECT_TYPE_NAME(tifffile->color_space));
 	g_free(input_filename);
 
 	return(TRUE);
