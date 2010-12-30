775b94797fde1e967bf6ea03a86a927a2744a6f8
akv <anders@kvistmail.dk>
2010-12-30 02:49:26 +0000

    Moved gui_dialog_* functions to new rs-gui-functions.c in librawstudio.

--- src/gtk-helper.c
+++ src/gtk-helper.c
@@ -30,6 +30,7 @@
 #include "filename.h"
 #include "gtk-helper.h"
 #include "rs-preview-widget.h"
+#include "rs-gui-functions.h"
 #include <gettext.h>
 #include <lcms.h>
 
@@ -543,60 +544,6 @@ cairo_convert_to_pixbuf (cairo_surface_t *surface)
 	}
 
 	return pixbuf;
-}
-
-GtkWidget *
-gui_dialog_make_from_text(const gchar *stock_id, gchar *primary_text, gchar *secondary_text)
-{
-	GtkWidget *secondary_label;
-
-	secondary_label = gtk_label_new (NULL);
-	gtk_label_set_line_wrap (GTK_LABEL (secondary_label), TRUE);
-	gtk_label_set_use_markup (GTK_LABEL (secondary_label), TRUE);
-	gtk_misc_set_alignment (GTK_MISC (secondary_label), 0.0, 0.5);
-	gtk_label_set_selectable (GTK_LABEL (secondary_label), TRUE);
-	gtk_label_set_markup (GTK_LABEL (secondary_label), secondary_text);
-	
-	return(gui_dialog_make_from_widget(stock_id, primary_text, secondary_label));
-}
-
-GtkWidget *
-gui_dialog_make_from_widget(const gchar *stock_id, gchar *primary_text, GtkWidget *widget)
-{
-	GtkWidget *dialog, *image, *hhbox, *vvbox;
-	GtkWidget *primary_label;
-	gchar *str;
-
-	image = gtk_image_new_from_stock(stock_id, GTK_ICON_SIZE_DIALOG);
-	gtk_misc_set_alignment (GTK_MISC (image), 0.5, 0.0);
-	dialog = gtk_dialog_new();
-	gtk_container_set_border_width (GTK_CONTAINER (dialog), 5);
-	gtk_box_set_spacing (GTK_BOX (GTK_DIALOG (dialog)->vbox), 14);
-	gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
-	gtk_dialog_set_has_separator (GTK_DIALOG (dialog), FALSE);
-	gtk_window_set_title (GTK_WINDOW (dialog), "");
-	gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
-	gtk_window_set_destroy_with_parent (GTK_WINDOW (dialog), TRUE);
-
-	primary_label = gtk_label_new (NULL);
-	gtk_label_set_line_wrap (GTK_LABEL (primary_label), TRUE);
-	gtk_label_set_use_markup (GTK_LABEL (primary_label), TRUE);
-	gtk_misc_set_alignment (GTK_MISC (primary_label), 0.0, 0.5);
-	gtk_label_set_selectable (GTK_LABEL (primary_label), TRUE);
-	str = g_strconcat("<span weight=\"bold\" size=\"larger\">", primary_text, "</span>", NULL);
-	gtk_label_set_markup (GTK_LABEL (primary_label), str);
-	g_free(str);
-
-	hhbox = gtk_hbox_new (FALSE, 12);
-	gtk_container_set_border_width (GTK_CONTAINER (hhbox), 5);
-	gtk_box_pack_start (GTK_BOX (hhbox), image, FALSE, FALSE, 0);
-	vvbox = gtk_vbox_new (FALSE, 12);
-	gtk_box_pack_start (GTK_BOX (hhbox), vvbox, FALSE, FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (vvbox), primary_label, FALSE, FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (vvbox), widget, FALSE, FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox), hhbox, FALSE, FALSE, 0);
-
-	return(dialog);
 }
 
 /**
