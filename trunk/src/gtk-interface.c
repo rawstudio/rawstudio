1bf9d453bf62d77b102e8077608d919aad40a32f
post <post@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2011-04-06 20:39:23 +0000

    Set default active type in quick export options.

--- trunk/src/gtk-interface.c
+++ trunk/src/gtk-interface.c
@@ -820,6 +820,8 @@ gui_make_preference_quick_export(void)
 	quick->event = filetype_event;
 	gui_confbox_set_callback(filetype_box, quick, filetype_changed);
 	active = gui_confbox_get_active(filetype_box);
+	if (!active)
+		active = g_type_from_name("RSJpegfile");
 	quick->output_type = g_type_name(GPOINTER_TO_INT(active));
 
 	/* Load default from conf, or use RSJpegfile */
