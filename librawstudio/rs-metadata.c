9422024a556d99649929dd1a3d4388a12ad6d7ce
post <klauspost@gmail.com>
2011-12-29 19:26:16 +0000

    Bump metadata version to re-read exposure compensation.

--- librawstudio/rs-metadata.c
+++ librawstudio/rs-metadata.c
@@ -117,7 +117,7 @@ rs_metadata_new (void)
 	return g_object_new (RS_TYPE_METADATA, NULL);
 }
 
-#define METACACHEVERSION 7
+#define METACACHEVERSION 8
 void
 rs_metadata_cache_save(RSMetadata *metadata, const gchar *filename)
 {
