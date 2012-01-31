3b9128f9f39c1690f34c62858eb77d001785d57e
post <post@ff961bb0-e316-0410-bf66-ce93e2ff3b84>
2012-01-31 17:44:02 +0000

    Remove unused code.

--- trunk/plugins/lensfun/lensfun.c
+++ trunk/plugins/lensfun/lensfun.c
@@ -89,12 +89,10 @@ static RSFilterResponse *get_image(RSFilter *filter, const RSFilterRequest *requ
 static void inline rs_image16_nearest_full(RS_IMAGE16 *in, gushort *out, gfloat *pos);
 static void inline rs_image16_bilinear_full(RS_IMAGE16 *in, gushort *out, gfloat *pos);
 extern gboolean is_sse2_compiled(void);
-extern void rs_image16_bilinear_full_sse2(RS_IMAGE16 *in, gushort *out, gfloat *pos, const gint *current_xy, const gint* min_max_xy);
 extern void rs_image16_bilinear_nomeasure_sse2(RS_IMAGE16 *in, gushort *out, gfloat *pos);
 extern gboolean is_sse4_compiled(void);
 extern void rs_image16_bilinear_nomeasure_sse4(RS_IMAGE16 *in, gushort *out, gfloat *pos);
 extern gboolean is_avx_compiled(void);
-extern void rs_image16_bilinear_full_avx(RS_IMAGE16 *in, gushort *out, gfloat *pos, const gint *current_xy, const gint* min_max_xy);
 extern void rs_image16_bilinear_nomeasure_avx(RS_IMAGE16 *in, gushort *out, gfloat *pos);
 static RSFilterClass *rs_lensfun_parent_class = NULL;
 
