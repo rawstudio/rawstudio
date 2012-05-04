e82dc2fb8852c37d6fd31b2bde8cefea442e5a88
post <klauspost@gmail.com>
2012-05-04 12:03:45 +0000

    Remeber to reference new previous filter, if replacing an earlier filter.

--- librawstudio/rs-filter.c
+++ librawstudio/rs-filter.c
@@ -138,8 +138,7 @@ rs_filter_set_previous(RSFilter *filter, RSFilter *previous)
 			filter->previous->next_filters = g_slist_remove(filter->previous->next_filters, filter);
 			g_object_unref(filter->previous);
 		}
-		else
-			filter->previous = g_object_ref(previous);
+		filter->previous = g_object_ref(previous);
 
 		previous->next_filters = g_slist_append(previous->next_filters, filter);
 	}
