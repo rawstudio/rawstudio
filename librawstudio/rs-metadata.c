282e0622a0697f9918f9fb9f0e725ee4fbd2b47c
Anders Brander <anders@brander.dk>
2014-11-28 20:46:44 +0100

    Added support for Canon 6D white balance.

--- librawstudio/rs-metadata.c
+++ librawstudio/rs-metadata.c
@@ -117,7 +117,7 @@ rs_metadata_new (void)
 	return g_object_new (RS_TYPE_METADATA, NULL);
 }
 
-#define METACACHEVERSION 10
+#define METACACHEVERSION 11
 void
 rs_metadata_cache_save(RSMetadata *metadata, const gchar *filename)
 {
