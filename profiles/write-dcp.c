d88832e491459d8fcf0a889046547bd6fff57012
Anders Brander <anders@brander.dk>
2015-02-19 15:14:08 +0100

    Added error checking, more comments and a bit of clean up.

--- profiles/write-dcp.c
+++ profiles/write-dcp.c
@@ -27,6 +27,7 @@
 #define DCPTAG_FORWARD_MATRIX1 0xc714
 #define DCPTAG_FORWARD_MATRIX2 0xc715
 
+/* Additional TIFF tags needed for writing DCP files */
 static const TIFFFieldInfo xtiffFieldInfo[] = {
 	{ DCPTAG_PROFILE_NAME,         -1, -1, TIFF_ASCII,     FIELD_CUSTOM,  TRUE, FALSE, "ProfileName" },
 	{ DCPTAG_PROFILE_EMBED_POLICY,  1,  1, TIFF_LONG,      FIELD_CUSTOM, FALSE, FALSE, "ProfileEmbedPolicy" },
@@ -35,6 +36,13 @@ static const TIFFFieldInfo xtiffFieldInfo[] = {
 	{ DCPTAG_FORWARD_MATRIX2,      -1, -1, TIFF_SRATIONAL, FIELD_CUSTOM, FALSE,  TRUE, "ForwardMatrix2" }
 };
 
+/**
+ * A version of atof() that isn't locale specific
+ * @note This doesn't do any error checking!
+ * @param str A NULL terminated string representing a number
+ * @return The number represented by str or 0.0 if str is NULL
+ * Liftet from librawstudio/rs-utils.h
+ */
 static gdouble
 rs_atof(const gchar *str)
 {
@@ -62,6 +70,11 @@ rs_atof(const gchar *str)
 	return result / div;
 }
 
+/**
+ * A version of atoi() that handles empty string and NULL arguments
+ * @param str A NULL terminated string representing an integer
+ * @return The integer represented by str or 0 if str is NULL or empty
+ */
 static gint
 rs_atoi(const gchar *str)
 {
@@ -74,18 +87,31 @@ rs_atoi(const gchar *str)
 	return atoi(str);
 }
 
+/**
+ * Read a 3x3 matrix from a XML file
+ * @param doc The XML document pointer
+ * @param cur Current node pointer
+ * @param A pointer to a gfloat[9] array
+ * @return TRUE if successfull read, FALSE otherwise.
+ */
 static gboolean
 read3x3matrix(xmlDocPtr doc, xmlNodePtr cur, gfloat *matrix)
 {
+	gint i;
 	gint row, col;
 	gint rows, cols;
 
 	rows = rs_atoi(xmlGetProp(cur, BAD_CAST "Rows"));
 	cols = rs_atoi(xmlGetProp(cur, BAD_CAST "Cols"));
 
+	for(i=0; i<9; i++)
+		matrix[i] = 0.0;
+
+	i = 0x0;
 	if (rows==3 && cols==3)
 	{
 		xmlNodePtr element = cur->xmlChildrenNode;
+
 		while(element)
 		{
 			if (!xmlStrcmp(element->name, BAD_CAST "Element"))
@@ -93,13 +119,21 @@ read3x3matrix(xmlDocPtr doc, xmlNodePtr cur, gfloat *matrix)
 				row = rs_atoi(xmlGetProp(element, BAD_CAST "Row"));
 				col = rs_atoi(xmlGetProp(element, BAD_CAST "Col"));
 				matrix[col + 3*row] = rs_atof(xmlNodeListGetString(doc, element->xmlChildrenNode, 1));
+
+				// Set a bit in i for each found member, we check before returning
+				i |= 1<<(col + 3*row);
 			}
 
 			element = element->next;
 		}
-		return TRUE;
 	}
-	return FALSE;
+
+	/* 0x1ff = 0b111111111 */
+
+	if (i != 0x1ff && i != 0x0)
+		g_warning("partial matrix read: %x", i);
+
+	return (i == 0x1ff);
 }
 
 int
@@ -117,41 +151,46 @@ main(gint argc, gchar **argv)
 		return 1;
 
 	tiff = TIFFOpen(argv[2], "w");
+	if (!tiff)
+		g_error("Failed to open \"%s\" for writing", argv[2]);
+
+	/* Add our custom DCP fields to TIFF */
 	TIFFMergeFieldInfo(tiff, xtiffFieldInfo, G_N_ELEMENTS(xtiffFieldInfo));
 
 	doc = xmlParseFile(argv[1]);
+	if (!doc)
+		g_error("Failed to open \"%s\" for reading", argv[1]);
+
 	cur = xmlDocGetRootElement(doc);
+
+	if (xmlStrcmp(cur->name, BAD_CAST "dcpData"))
+		g_error("Unexpected root tag [%s] in \"%s\", aborting", cur->name, argv[1]);
+
 	cur = cur->xmlChildrenNode;
 
 	while(cur)
 	{
 		if ((!xmlStrcmp(cur->name, BAD_CAST "text")))
 		{
-			// ignore this
+			/* ignore this */
 		}
 		else if ((!xmlStrcmp(cur->name, BAD_CAST "ProfileName")))
 		{
 			val = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 			if (val && *val)
-			{
 				TIFFSetField(tiff, DCPTAG_PROFILE_NAME, val);
-			}
 		}
 		else if ((!xmlStrcmp(cur->name, BAD_CAST "CalibrationIlluminant1")))
 		{
 			val = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 			if (rs_atoi(val) > 0)
-			{
 				TIFFSetField(tiff, TIFFTAG_CALIBRATIONILLUMINANT1, rs_atoi(val));
-			}
 		}
 		else if ((!xmlStrcmp(cur->name, BAD_CAST "CalibrationIlluminant2")))
 		{
 			val = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 			if (rs_atoi(val) > 0)
-			{
 				TIFFSetField(tiff, TIFFTAG_CALIBRATIONILLUMINANT2, rs_atoi(val));
-			}
 		}
 		else if ((!xmlStrcmp(cur->name, BAD_CAST "ColorMatrix1")))
 		{
@@ -187,31 +226,29 @@ main(gint argc, gchar **argv)
 		{
 			val = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 			if (rs_atoi(val) > 0)
-			{
 				TIFFSetField(tiff, DCPTAG_PROFILE_EMBED_POLICY, rs_atoi(val));
-			}
 		}
 		else if ((!xmlStrcmp(cur->name, BAD_CAST "UniqueCameraModelRestriction")))
 		{
 			val = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 			if (val)
-			{
 				TIFFSetField(tiff, TIFFTAG_UNIQUECAMERAMODEL, val);
-			}
 		}
 		else if ((!xmlStrcmp(cur->name, BAD_CAST "Copyright")))
 		{
 			val = xmlNodeListGetString(doc, cur->xmlChildrenNode, 1);
 			if (val)
-			{
 				TIFFSetField(tiff, DCPTAG_PROFILE_COPYRIGHT, val);
-			}
 		}
 		else
-			printf("%s is unsupported\n", cur->name);
+			g_warning("[%s] tag in \"%s\" is unsupported", cur->name, argv[1]);
 
 		cur = cur->next;
-	}	
+	}
 
+	/* Close up shop */
+	xmlFreeDoc(doc);
 	TIFFClose(tiff);
+
+	return 0;
 }
