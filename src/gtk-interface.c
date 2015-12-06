1377e9385cb69f68bae5c38d89c78fab4029bd62
Sérgio M. Basto <sergio@serjux.com>
2015-12-06 13:28:49 +0000

    Use GSIZE_TO_POINTER and GPOINTER_TO_SIZE for GType
    GPOINTER_TO_UINT may truncate the GType (actually gsize, i.e. unsigned
    long).
    Use rather GPOINTER_TO_SIZE() to be safe. And GSIZE_TO_POINTER() for the
    opposite conversion.
    
    https://git.gnome.org/browse/network-manager-applet/commit/?id=ca9bed776b1c69b78c42f8c025e3d4f7d548762f

--- src/gtk-interface.c
+++ src/gtk-interface.c
@@ -875,8 +875,8 @@ gui_make_preference_quick_export(void)
 	gui_confbox_set_callback(filetype_box, quick, filetype_changed);
 	active = gui_confbox_get_active(filetype_box);
 	if (!active)
-		active = GUINT_TO_POINTER(g_type_from_name("RSJpegfile"));
-	quick->output_type = g_type_name(GPOINTER_TO_INT(active));
+		active = GSIZE_TO_POINTER(g_type_from_name("RSJpegfile"));
+	quick->output_type = g_type_name((GType) GPOINTER_TO_SIZE(active));
 
 	/* Load default from conf, or use RSJpegfile */
 	gui_confbox_load_conf(filetype_box, "RSJpegfile");
